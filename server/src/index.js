b8e85e5cf3c8af4b6ead3165aec97db54b7ad4c5
Phạm Leos <baoph21411@st.uel.edu.vn>
2024-12-27 23:07:18 +0700

    fix index.js

--- server/src/index.js
+++ server/src/index.js
@@ -33,7 +33,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(_dirname, "/client/dist")));
+  app.use(express.static(path.join(_dirname, "../client/dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(_dirname, "../client", "dist", "index.html"));
